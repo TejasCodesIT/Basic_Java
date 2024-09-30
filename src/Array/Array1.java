0cd1913df7eae34a75963089b6c4f420b8de4575
Wakchaure123 <133526230+Wakchaure123@users.noreply.github.com>
2024-09-30 12:35:06 +0530

    Array folder creation

---
+++ src/Array/Array1.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Array1 {
+			
+	
+	public static void main(String[] args) {
+		
+		int [] arr1;  // declaration
+		arr1 = new int[10] ;
+		
+		for(int i=0;i<arr1.length;i++) {
+			
+			arr1[i]=i+1;
+			System.err.println(arr1[i]);
+		}
+		
+	}
+}
