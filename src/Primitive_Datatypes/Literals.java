6ee44b3443d7c67f5aa998da69fbe1eab73388af
Wakchaure123 <133526230+Wakchaure123@users.noreply.github.com>
2024-09-30 12:21:00 +0530

    Secound commit

--- src/Primitive_Datatypes/Literals.java
+++ src/Primitive_Datatypes/Literals.java
@@ -3,7 +3,7 @@ package Primitive_Datatypes;
 public class Literals {
 			public static void main(String[] args) {
 				
-				
+				int c;
 				int octalNumber = 064;
 				System.out.println("Octal Number :-"+octalNumber);
 				
@@ -11,7 +11,7 @@ public class Literals {
 				System.out.println("Octal Number :-"+hexadecimal);
 				
 				int binary = 0b110100;
-				System.out.println("Octal Number :-"+binary);
+				System.out.println("Octal N umber :-"+binary);
 				
 				
 			}
