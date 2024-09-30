a4f159678c781a447cc79ffbfec1f34889675e30
Wakchaure123 <133526230+Wakchaure123@users.noreply.github.com>
2024-09-30 11:47:09 +0530

    First Commit

---
+++ src/Primitive_Datatypes/Formate.java
@@ -0,0 +1,14 @@
+package Primitive_Datatypes;
+
+public class Formate {
+			
+		public static void main(String[] args) {
+			
+			int intNumber = 25_15424_4;
+			byte byteNumber= 12_1;
+			int hexadecimal = 0x34;
+			System.out.println("int value");
+			System.out.println("Byte Number :- ");
+			
+		}
+}
---
+++ src/Primitive_Datatypes/Literals.java
@@ -0,0 +1,18 @@
+package Primitive_Datatypes;
+
+public class Literals {
+			public static void main(String[] args) {
+				
+				
+				int octalNumber = 064;
+				System.out.println("Octal Number :-"+octalNumber);
+				
+				int hexadecimal = 0x34;
+				System.out.println("Octal Number :-"+hexadecimal);
+				
+				int binary = 0b110100;
+				System.out.println("Octal Number :-"+binary);
+				
+				
+			}
+}
